3a4e064d87ecf4c0cdc6615875d2af94263ec2a3
Thomas Axelsson <thomasa88@gmail.com>
2020-07-31 07:10:19 +0200

    Base structure

--- AnyShortcut.py
+++ AnyShortcut.py
@@ -3,24 +3,40 @@
 
 import adsk.core, adsk.fusion, adsk.cam, traceback
 
-def run(context):
-    ui = None
-    try:
-        app = adsk.core.Application.get()
-        ui  = app.userInterface
-        ui.messageBox('Hello addin')
+import os
 
-    except:
-        if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+NAME = 'Any Shortcut'
+FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+# Import relative path to avoid namespace pollution
+from .thomasa88lib import utils
+from .thomasa88lib import events
+from .thomasa88lib import manifest
+from .thomasa88lib import error
+from .thomasa88lib import settings
+
+# Force modules to be fresh during development
+import importlib
+importlib.reload(thomasa88lib.utils)
+importlib.reload(thomasa88lib.events)
+importlib.reload(thomasa88lib.manifest)
+importlib.reload(thomasa88lib.error)
+importlib.reload(thomasa88lib.settings)
+
+app_ = None
+ui_ = None
+events_manager_ = thomasa88lib.events.EventsManager(NAME)
+error_catcher_ = thomasa88lib.error.ErrorCatcher()
+manifest_ = thomasa88lib.manifest.read()
+settings_ = thomasa88lib.settings.SettingsManager({})
+
+def run(context):
+    global app_
+    global ui_
+    with error_catcher_:
+        app_ = adsk.core.Application.get()
+        ui_ = app_.userInterface
 
 def stop(context):
-    ui = None
-    try:
-        app = adsk.core.Application.get()
-        ui  = app.userInterface
-        ui.messageBox('Stop addin')
-
-    except:
-        if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+    with error_catcher_:
+        pass
