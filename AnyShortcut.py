9284d1c2a24434eb61dc1a3bc4621f82f91ec42c
Thomas Axelsson <thomasa88@gmail.com>
2020-07-31 06:16:13 +0200

    Initial commit

---
+++ AnyShortcut.py
@@ -0,0 +1,26 @@
+#Author-Thomas Axelsson
+#Description-Shows a menu that let's you assign shortcuts to your last run commands.
+
+import adsk.core, adsk.fusion, adsk.cam, traceback
+
+def run(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        ui.messageBox('Hello addin')
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+def stop(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        ui.messageBox('Stop addin')
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
